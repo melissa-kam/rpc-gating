13e9af1204b0d70b3e47923cb5d23d35b94495b3
Melissa Kam <melissa.kam@rackspace.com>
2017-01-24 10:39:53 -0600

    temp

--- pipeline-steps/run_script.groovy
+++ pipeline-steps/run_script.groovy
@@ -1,45 +1,9 @@
-def call(Map args){
-    withEnv(args.environment_vars){
-
+def call(Map args) {
+    withEnv(args.environment_vars) {
+        sh """
+        #!/bin/bash
+        sudo -E ./${args.script}
+        """
     }
-
-
-
 }
-
-
-def call2(Map args){
-    common = load './rpc-gating/pipeline-steps/common.groovy'
-    region_env = "RAX_REGION=${args.region}"
-    withEnv(['ANSIBLE_FORCE_COLOR=true', region_env]){
-        withCredentials([
-                file(
-                        credentialsId: 'RPCJENKINS_RAXRC',
-                        variable: 'RAX_CREDS_FILE'
-                ),
-                file(
-                        credentialsId: 'id_rsa_cloud10_jenkins_file',
-                        variable: 'JENKINS_SSH_PRIVKEY'
-                )
-        ]){
-            dir("rpc-gating/playbooks"){
-                common.install_ansible()
-                common.venvPlaybook(
-                        venv: ".venv",
-                        args: [
-                                "-e name=\"${args.nodeprefix}\"",
-                                "-e flavor=\"${args.flavor}\"",
-                                "-e image=\"${args.image}\"",
-                                "-e count=\"${args.numnodes}\"",
-                                "-e keyname=\"${args.keyname}\"",
-                                "--private-key=\"${env.JENKINS_SSH_PRIVKEY}\"",
-                                "allocate_pubcloud.yml"
-                        ]
-                )
-                return result
-
-            } // directory
-        } //withCredentials
-    } // withEnv
-} //call
 return this
